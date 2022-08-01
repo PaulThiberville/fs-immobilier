e66d54e0362eebdc05c8ed304b873beeae512673
Paul Thiberville <paul.thiberville@live.fr>
2022-08-01 10:32:54 +0200

    heroku rebuild

--- controllers/product.js
+++ controllers/product.js
@@ -35,6 +35,7 @@ exports.readAll = async (req, res) => {
 exports.readOne = async (req, res) => {
   try {
     const product = await Product.findOne({ _id: req.params.id });
+    if (!product) return res.status(404).json({ error: "Product not found" });
     return res.status(200).json(product);
   } catch (error) {
     return res.status(500).json({ error: error.message });
